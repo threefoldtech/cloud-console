90e41741cdf80a2e88e69d17c97be8a96dce7f87
Lee Smet <lee.smet@hotmail.com>
2022-11-10 17:24:24 +0100

    Working binary
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,192 @@
+use std::sync::Arc;
+use tokio::{
+    io::{AsyncWrite, AsyncWriteExt},
+    sync::mpsc,
+};
+
+const CONNECTION_BUFFER: usize = 1000;
+
+/// An internal console buffer, multiplexing to multiple outputs. The size of the buffer is a
+/// constant parameter.
+pub struct ConsoleMux<const H: usize> {
+    data: [u8; H],
+    head: usize,
+    remotes: Vec<mpsc::Sender<Arc<Vec<u8>>>>,
+}
+
+impl<const H: usize> ConsoleMux<H> {
+    /// Create a new ConsoleMux with no data.
+    // TODO: implement a cleanup loop for dropped receivers, this now only happens when data is
+    // send and can cause a theoretical buildup.
+    pub fn new() -> ConsoleMux<H> {
+        ConsoleMux {
+            data: [0; H],
+            head: 0,
+            remotes: Vec::new(),
+        }
+    }
+
+    /// Writes data to the console. The last H bytes of data are retained in the internal buffer
+    /// and will be served to new clients when they connect
+    pub fn write_data(&mut self, data: &[u8]) {
+        // Only keep the data we can actually write to the buffer.
+        let sized_data = if data.len() > H {
+            &data[data.len() - H..]
+        } else {
+            data
+        };
+
+        // Write data to buffer from head -> end of buffer
+        let remainder = H - self.head;
+        let to_write = usize::min(remainder, sized_data.len());
+        self.data[self.head..self.head + to_write].copy_from_slice(&sized_data[..to_write]);
+        // Now write remainder of data to start of buffer -> head
+        if sized_data.len() > remainder {
+            self.data[..sized_data.len() - remainder].copy_from_slice(&sized_data[to_write..]);
+        }
+
+        // Update head
+        self.head = (self.head + sized_data.len()) % H;
+
+        // Write data to connected endpoints, but check if there are any first. This avoids a heap
+        // allocation if it is not needed.
+        if self.remotes.is_empty() {
+            return;
+        }
+
+        let msg = Arc::new(Vec::from(data));
+
+        // Importantly we do a try send here to avoid blocking. If the channel is full, the remote
+        // is lagging and we drop the message. This will likely cause a disconnect and reconnect
+        // later. If the remote is disconnected it means it is gone entirely.
+        self.remotes.retain(|remote| {
+            !matches!(
+                remote.try_send(msg.clone()),
+                Err(mpsc::error::TrySendError::Closed(_))
+            )
+        });
+    }
+
+    /// Attach a new remote, which will receive data every time a write happens on console mux.
+    /// The future returned by this function completes as soon as the existing buffer is sent to
+    /// the remote. Errors encountered during writing at any point will not be propagated, instead
+    /// they will simply stop the processing of data. After an error writing to the remote, no new
+    /// data will be written to the remote.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic when executed outside the scope of a [`tokio::runtime::Runtime`]
+    pub async fn attach_remote<R>(&mut self, mut remote: R)
+    where
+        R: AsyncWrite + Unpin + Send + 'static,
+    {
+        let (tx, mut rx) = mpsc::channel(CONNECTION_BUFFER);
+        self.remotes.push(tx);
+
+        // Write the contents of the existing buffer
+        if let Err(e) = remote.write_all(&self.data[self.head..]).await {
+            eprintln!("Error writing first half of data buffer to remote {}", e);
+            return;
+        }
+        if let Err(e) = remote.write_all(&self.data[..self.head]).await {
+            eprintln!("Error writing second half of data buffer to remote {}", e);
+            return;
+        }
+
+        // Spawn data forwarding loop.
+        tokio::spawn(async move {
+            while let Some(data) = rx.recv().await {
+                // If we encounter an error writing to the remote, treat it as fatal. Also, use
+                // write_all as a convenience here.
+                if let Err(e) = remote.write_all(&data).await {
+                    eprintln!("Error writing to remote {}", e);
+                    break;
+                }
+            }
+        });
+    }
+
+    /// Attach a new channel sender to the console, which will be used to notify the receiver of
+    /// all sends. The future returned by this function completes as soon as the existing buffer
+    /// has been send on the channel. If there is sufficient capacity, this will not wait for the
+    /// data to be consumed.
+    ///
+    /// This function is intended as a convenience function in case the caller does not have a type
+    /// compatible with [`AsyncWrite`] to call [`ConsoleMux::attach_remote`].
+    ///
+    /// # Panics
+    ///
+    /// This function will panic when executed outside the scope of a [`tokio::runtime::Runtime`]
+    pub async fn attach_channel(&mut self, tx: mpsc::Sender<Arc<Vec<u8>>>) {
+        // Write the contents of the existing buffer
+        if let Err(e) = tx.send(Arc::new(Vec::from(&self.data[self.head..]))).await {
+            eprintln!("Error writing first half of data buffer to channel {}", e);
+            return;
+        }
+        if let Err(e) = tx.send(Arc::new(Vec::from(&self.data[..self.head]))).await {
+            eprintln!("Error writing second half of data buffer to channel {}", e);
+            return;
+        }
+
+        self.remotes.push(tx);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mux_write_no_rotation() {
+        let mut cm = ConsoleMux::<200>::new();
+
+        let data = vec![1; 150];
+        cm.write_data(&data);
+
+        assert_eq!(cm.head, 150);
+        assert_eq!(cm.data[149], 1);
+        assert_eq!(cm.data[150], 0);
+    }
+
+    #[test]
+    fn test_mux_write_with_rotation() {
+        let mut cm = ConsoleMux::<200>::new();
+
+        let data = vec![1; 150];
+        cm.write_data(&data);
+
+        let data = vec![2; 90];
+        cm.write_data(&data);
+
+        assert_eq!(cm.head, 40);
+        assert_eq!(cm.data[39], 2);
+        assert_eq!(cm.data[40], 1);
+        assert_eq!(cm.data[149], 1);
+        assert_eq!(cm.data[150], 2);
+    }
+
+    #[test]
+    fn test_mux_write_large_buffer_no_rotation() {
+        let mut cm = ConsoleMux::<100>::new();
+
+        let data = vec![1; 150];
+        cm.write_data(&data);
+
+        assert_eq!(cm.head, 0);
+        assert_eq!(&cm.data, vec![1; 100].as_slice());
+    }
+
+    #[test]
+    fn test_mux_write_large_buffer_with_rotation() {
+        let mut cm = ConsoleMux::<100>::new();
+
+        let data = vec![1; 50];
+        cm.write_data(&data);
+
+        let data = vec![2; 125];
+        cm.write_data(&data);
+
+        assert_eq!(cm.head, 50);
+        assert_eq!(&cm.data, vec![2; 100].as_slice());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,55 @@
 use axum::{
+    body::{boxed, Full},
     extract::ws::{WebSocket, WebSocketUpgrade},
-    response::Response,
+    http::{header, StatusCode, Uri},
+    response::{IntoResponse, Response},
     routing::get,
     Extension, Router,
 };
-use futures::{
-    sink::SinkExt,
-    stream::{SplitSink, SplitStream, StreamExt},
-};
+use cloud_console::ConsoleMux;
+use futures::{sink::SinkExt, stream::StreamExt};
+use rust_embed::RustEmbed;
 use tokio::{
     fs::OpenOptions,
-    io::{self, AsyncReadExt, AsyncWriteExt},
+    io::{AsyncReadExt, AsyncWriteExt},
+    sync::{mpsc, Mutex},
 };
+use tower_http::compression::CompressionLayer;
 
-use std::sync::{Arc, Mutex};
+use std::{net::SocketAddr, sync::Arc};
 
+/// 80 columns, 2000 rows. Technically the Mux does not track rows but just a byte array. This is
+///    a sane default as such: a single column can contain up to 4 bytes (since it is unicode),
+///    however not all rows will be completely filled. Most will indeed only be partially used.
+const CONSOLE_BUFFER: usize = 80 * 2000;
+/// Amount of data fragments from remotes to buffer while forwarding to the pty. If there are more
+/// than this amount queued, new writes from remotes will block. Not sure if this is even needed.
+const WRITE_BACKLOG: usize = 100;
+
+#[derive(RustEmbed)]
+#[folder = "frontend/public"]
+struct Assets;
+
+/// Application shared state between handlers.
+#[derive(Clone)]
 struct State {
-    buffer: Mutex<[[u8; 80]; 1000]>,
-    row: usize,
-    pos: (u8, u8),
-    height: usize,
-    width: usize,
+    inner: Arc<Mutex<ConsoleMux<CONSOLE_BUFFER>>>,
+    data_sender: mpsc::Sender<Vec<u8>>,
 }
 
 impl State {
-    fn new() -> State {
+    /// Create a new State with a default iniitalized ConsoleMux and the given channel write half
+    /// to forward data to the pty.
+    pub fn new(data_sender: mpsc::Sender<Vec<u8>>) -> State {
         State {
-            buffer: Mutex::new([[0; 80]; 1000]),
-            row: 0,
-            pos: (0, 0),
-            height: 25,
-            width: 80,
+            inner: Arc::new(Mutex::new(ConsoleMux::new())),
+            data_sender,
         }
+    }
+
+    /// Retrieve a reference to the ConsoleMux.
+    pub fn console(&self) -> Arc<Mutex<ConsoleMux<CONSOLE_BUFFER>>> {
+        self.inner.clone()
     }
 }
 
@@ -40,114 +58,192 @@ async fn main() {
     let mut args = std::env::args();
     // Binary name - ignore
     let _ = args.next();
-    let pty = args.next();
-    let bind_ip = args.next();
-    let bind_port = args.next();
-    //     if pty.is_none() || bind_ip.is_none() || bind_port.is_none() {
-    //         eprintln!(
-    //             r#"Cloud console - An interactive web based terminal connected to a pty
-    // Usage:
-    //     cloud-console <path_to_pty> <bind_ip> <bind_port>"#
-    //         );
-    //         std::process::exit(1);
-    //     }
-    // let mut handles = Vec::new();
+    let pty = if let Some(pty) = args.next() {
+        pty
+    } else {
+        print_usage_and_exit()
+    };
+    let bind_ip = if let Some(bind_ip) = args.next() {
+        bind_ip
+    } else {
+        print_usage_and_exit()
+    };
+    let bind_port = if let Some(bind_port) = args.next() {
+        bind_port
+    } else {
+        print_usage_and_exit()
+    };
+    let addr = SocketAddr::new(bind_ip.parse().unwrap(), bind_port.parse().unwrap());
 
-    // handles.push(tokio::spawn(async move {
-    //     let mut stdout = io::stdout();
-    //     io::copy(&mut reader, &mut stdout).await
-    //     //    let mut buf = [0; 80];
-    //     //    loop {
-    //     //        let n = reader.read(&mut buf).await.unwrap();
-    //     //        eprintln!("Read {} bytes from pts", n);
-    //     //        stdout.write(&buf[..n]).await.unwrap();
-    //     //        eprintln!("Wrote {} bytes to stdout", n);
-    //     //    }
-    // }));
-
-    // handles.push(tokio::spawn(async move {
-    //     let mut stdin = io::stdin();
-    //     io::copy(&mut stdin, &mut writer).await
-    //     // let mut buf = [0; 80];
-    //     // loop {
-    //     //     let n = stdin.read(&mut buf).await.unwrap();
-    //     //     eprintln!("Read {} bytes from stdin", n);
-    //     //     writer.write(&buf[..n]).await.unwrap();
-    //     //     eprintln!("Wrote {} bytes to pty", n);
-    //     // }
-    // }));
-
-    let app = Router::new()
-        .route("/ws", get(handler))
-        .layer(Extension(Arc::new(State::new())));
-
-    //tokio::task::spawn(async move {
-    axum::Server::bind(&"[::]:9999".parse().unwrap())
-        .serve(app.into_make_service())
+    // Open the pty file handle twice, one for reading and one for writing. Opening it in both read
+    // + write, then calling `.split()` on it seems to resuld in a deadlock somehow.
+    let mut reader = OpenOptions::new()
+        .read(true)
+        .write(false)
+        .create(false)
+        .truncate(false)
+        .open(&pty)
         .await
         .unwrap();
-    //});
-    // for handle in handles {
-    //     handle.await.unwrap().unwrap();
-    //     //handle.await.unwrap();
-    // }
-}
+    let mut writer = OpenOptions::new()
+        .read(false)
+        .write(true)
+        .create(false)
+        .truncate(false)
+        .open(pty)
+        .await
+        .unwrap();
 
-async fn handler(ws: WebSocketUpgrade, Extension(state): Extension<Arc<State>>) -> Response {
-    ws.on_upgrade(|socket| handle_socket(socket, state))
-}
+    let (tx, mut rx) = mpsc::channel::<Vec<u8>>(WRITE_BACKLOG);
 
-async fn handle_socket(socket: WebSocket, state: Arc<State>) {
-    let (mut sender, receiver) = socket.split();
-    tokio::spawn({
-        let state = state.clone();
-        async move {
-            let mut reader = OpenOptions::new()
-                .read(true)
-                .write(false)
-                .create(false)
-                .truncate(false)
-                .open("/dev/pts/2")
-                .await
-                .unwrap();
-            loop {
-                let mut buf = vec![0; 320];
-                let n = reader.read(&mut buf).await.unwrap();
-                buf.truncate(n);
-                sender
-                    .send(axum::extract::ws::Message::Binary(buf))
-                    .await
-                    .unwrap();
+    // Loop to forward console data to pty.
+    tokio::spawn(async move {
+        while let Some(data) = rx.recv().await {
+            if let Err(e) = writer.write_all(&data).await {
+                eprintln!("Could not forward data to pty {}", e);
+                return;
             }
         }
     });
 
+    let state = State::new(tx);
+    let console = state.console();
+    // Loop to forward pty data to console mux
+    tokio::spawn(async move {
+        // TODO: good buffer size?
+        let mut buffer = [0; 320];
+        loop {
+            let n = match reader.read(&mut buffer).await {
+                Ok(n) => n,
+                Err(e) => {
+                    // TODO: This is actually fatal. In an ideal world, we would close down the
+                    // console mux,flushing all pending data to remotes, then exit the application.
+                    // Might have to add a flag on state which prevents new connections.
+                    eprintln!("Could not read from pty {}", e);
+                    return;
+                }
+            };
+            // Forward data to console mux.
+            console.lock().await.write_data(&buffer[..n]);
+        }
+    });
+
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/ws", get(handler))
+        .fallback(get(static_handler))
+        .layer(CompressionLayer::new())
+        .layer(Extension(state));
+
+    //tokio::task::spawn(async move {
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler(ws: WebSocketUpgrade, Extension(state): Extension<State>) -> Response {
+    ws.on_upgrade(|socket| handle_socket(socket, state))
+}
+
+async fn handle_socket(socket: WebSocket, state: State) {
+    // Split socket in a tx and rx pair.
+    let (mut sender, receiver) = socket.split();
+    // Attach tx pair to console.
+    // Since SplitSink only implements futures-sink::Sink, we need a converter. Do in-memory for
+    // now.
+    // TODO: good channel capacity;
+    let (tx, mut rx) = mpsc::channel::<Arc<Vec<u8>>>(1000);
+
+    tokio::spawn(async move {
+        while let Some(buf) = rx.recv().await {
+            if let Err(e) = sender
+                .send(axum::extract::ws::Message::Binary(buf.to_vec()))
+                .await
+            {
+                eprintln!("Could not send buffer to websocket {}", e);
+                // Try to close the socket so the other half is also closed for automatic cleanup.
+                // We don't care about errors here
+                let _ = sender.close().await;
+                return;
+            };
+        }
+    });
+    state.inner.lock().await.attach_channel(tx).await;
+
     tokio::spawn({
         async move {
-            let mut writer = OpenOptions::new()
-                .read(false)
-                .write(true)
-                .create(false)
-                .truncate(false)
-                .open("/dev/pts/2")
-                .await
-                .unwrap();
             receiver
                 .for_each(|msg| async {
-                    let mut writer = writer.try_clone().await.unwrap();
                     if let Ok(msg) = msg {
                         match msg {
                             axum::extract::ws::Message::Binary(d) => {
-                                writer.write(&d).await.unwrap();
+                                if let Err(e) = state.data_sender.send(d).await {
+                                    eprintln!("Could not send data to pty forwarder {}", e);
+                                    return;
+                                }
                             }
                             axum::extract::ws::Message::Text(t) => {
-                                writer.write(&t.as_bytes()).await.unwrap();
+                                if let Err(e) = state.data_sender.send(t.into_bytes()).await {
+                                    eprintln!("Could not send data to pty forwarder {}", e);
+                                    return;
+                                }
                             }
-                            _ => {}
+                            m => {
+                                eprintln!("Unsupported websocket message {:?}", m);
+                            }
                         };
                     };
                 })
                 .await;
         }
     });
+}
+
+/// Prints usage instructions to stdandard error, and exists the process with an error code.
+fn print_usage_and_exit() -> ! {
+    eprintln!(
+        r#"Cloud console - An interactive web based terminal connected to a pty
+    Usage:
+        cloud-console <path_to_pty> <bind_ip> <bind_port>"#
+    );
+    std::process::exit(1);
+}
+
+/// Handle index
+async fn index() -> impl IntoResponse {
+    static_handler("/index.html".parse::<Uri>().unwrap()).await
+}
+
+/// Handle static files
+async fn static_handler(uri: Uri) -> impl IntoResponse {
+    let path = uri.path().trim_start_matches('/').to_string();
+
+    StaticFile(path)
+}
+
+pub struct StaticFile<T>(pub T);
+
+impl<T> IntoResponse for StaticFile<T>
+where
+    T: Into<String>,
+{
+    fn into_response(self) -> Response {
+        let path = self.0.into();
+
+        match Assets::get(path.as_str()) {
+            Some(content) => {
+                let body = boxed(Full::from(content.data));
+                let mime = mime_guess::from_path(path).first_or_octet_stream();
+                Response::builder()
+                    .header(header::CONTENT_TYPE, mime.as_ref())
+                    .body(body)
+                    .unwrap()
+            }
+            None => Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(boxed(Full::from("404")))
+                .unwrap(),
+        }
+    }
 }
